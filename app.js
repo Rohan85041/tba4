8e4132d42fe9446dc40903840b6953e01fb7c2e3
Rohan85041 <chrohankumar85041@gmail.com>
2025-09-04 16:23:11 +0530

    telegram bot files

---
+++ app.js
@@ -0,0 +1,14 @@
+const {Telegraf} =require('telegraf');
+const dotenv=require('dotenv');
+dotenv.config();
+const bot=new Telegraf(process.env.token);
+bot.start(function(ctx){
+ctx.reply("hi");
+})
+bot.command('location',function(ctx){
+    ctx.replyWithLocation(15.3692,75.1217)
+})
+bot.command('meme',async function(ctx){
+   await ctx.replyWithAnimation('https://media1.tenor.com/m/Dc7Jf45wXjkAAAAd/upendra-gif.gif')
+})
+bot.launch();
